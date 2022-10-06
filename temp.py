739efe69416dad639e6dbc2cae1bd16cf8a3abe1
Envel-Nikita-Gutsenkov <52075465+Envel-Nikita-Gutsenkov@users.noreply.github.com>
2022-10-06 16:25:21 +0300

    5

--- temp.py
+++ temp.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+'''import tkinter as tk
 
 # Create the master object
 master = tk.Tk()
@@ -15,4 +15,17 @@ button2 = tk.Button(master, text="Button 2")
 button2.grid(columnspan=4, row=4, column=0)
 
 # The mainloop
-tk.mainloop()
+tk.mainloop()'''
+import tkinter as tk
+
+window = tk.Tk()
+
+for i in range(10):
+        frame = tk.Frame(master=window)
+        frame.grid(row=i, column=1, padx=5, pady=5)
+        label = tk.Entry(master=frame, width=10)
+        label2 = tk.Label(master=frame, text=f"Row {i}\nColumn {1}")
+        label.pack(padx=5, pady=5)
+        label2.pack(padx=5, pady=5)
+
+window.mainloop()
