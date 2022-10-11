b5f46acd10378f08243ea9b6176f66be403df5e5
Envel-Nikita-Gutsenkov <52075465+Envel-Nikita-Gutsenkov@users.noreply.github.com>
2022-10-11 12:51:13 +0300

    6

--- z6.py
+++ z6.py
@@ -5,23 +5,30 @@ z1 = []
 filepath = askopenfilename
 mylist = []
 
+
+# открыть (файл)
 def open_file():
     global filepath, mylist
+    # выбор файла
     filepath = askopenfilename(
         filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
     )
     if not filepath:
         return
+    # стирание, если был уже открыт файл
+    txt_edit.configure(state='normal')
     txt_edit.delete("1.0", tk.END)
+    # открытие и вызов вычислений
     with open(filepath, mode="r", encoding="utf-8") as input_file:
-        text = input_file.read()
-        txt_edit.insert(tk.END, text)
+        txt_edit.insert(tk.END, input_file.read())
+        txt_edit.configure(state='disabled')
         input_file.seek(0)
         mylist = input_file.readlines()
     window.title(f"{filepath}")
     to_memory(mylist)
 
 
+# расчет (запись в файл)
 def save_file(mylist):
     global filepath
     # записываем изменения
@@ -29,47 +36,17 @@ def save_file(mylist):
         output_file.writelines([str(i) for i in mylist])
     output_file.close()
     # перечитываем файл, чтобы отобразить изменения
-    with open(filepath, mode="r", encoding="utf-8") as input_file:
-        text = input_file.read()
-        txt_edit.insert(tk.END, text)
-
-
-
-# подготавливает к записи нужные подсчитанные строки
-def savedata():
-    for i in range(7, 15):
-        mylist[i] = mylist[i].strip('\n')
-        print("беру ", mylist[i], " и вношу туда \n", mylist[i] + str(z1[i - 1]) + "р.")
-        mylist[i] = mylist[i] + str(z1[i - 1]) + " р.\n"
-    print(mylist)
-    save_file(mylist)
-
-# сумма стр. 1–9
-def str10():
-    total = 0
-    for i in range(0, 8):
-        total = total + z1[i]
-    return total
-
-
-# подсчет необходимых частей
-def calculate():
-    global z1
-    z1.append(z1[5] * 35 / 100)
-    z1.append(z1[5] / 100)
-    z1.append(z1[5] * 4 / 100)
-    z1.append(str10())
-    z1.append(z1[9] * 5 / 100)
-    z1.append(z1[9] + z1[10])
-    z1.append(z1[11] * 20 / 100)
-    z1.append(z1[11] + z1[12])
-    print(z1)
+    txt_edit.configure(state='normal')
+    txt_edit.delete("1.0", tk.END)
+    with open(filepath, mode="r", encoding="utf-8") as output_file:
+        txt_edit.insert(tk.END, output_file.read())
+        txt_edit.configure(state='disabled')
+    output_file.close()
 
 
 # запись значений в память для вычислений
 def to_memory(mylist):
     global z1
-    print(mylist)
     for i in range(1, 7):
         z1.append(float(extractor(mylist[i])))
     calculate()
@@ -83,10 +60,38 @@ def extractor(pstring):
     for m in splitting:
         if m.isdigit():
             result = result + m
-    print("Получил число путем извлечения: " + result)
     return result
 
 
+# подготавливает к записи нужные подсчитанные строки
+def savedata():
+    for i in range(7, 15):
+        mylist[i] = mylist[i].strip('\n')
+        mylist[i] = mylist[i] + str(z1[i - 1]) + " р.\n"
+    save_file(mylist)
+
+
+# подсчет необходимых частей
+def calculate():
+    global z1
+    z1.append(z1[5] * 35 / 100)
+    z1.append(z1[5] / 100)
+    z1.append(z1[5] * 4 / 100)
+    z1.append(str10())
+    z1.append(z1[9] * 5 / 100)
+    z1.append(z1[9] + z1[10])
+    z1.append(z1[11] * 20 / 100)
+    z1.append(z1[11] + z1[12])
+
+
+# сумма стр. 1–9 для п.10
+def str10():
+    total = 0
+    for i in range(0, 8):
+        total = total + z1[i]
+    return total
+
+# графический интерфейс
 window = tk.Tk()
 window.title("Лабораторная работа 6")
 
