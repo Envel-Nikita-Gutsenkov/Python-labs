913009b72f0937d4b59d167a473f23ff91d3fc39
Envel-Nikita-Gutsenkov <52075465+Envel-Nikita-Gutsenkov@users.noreply.github.com>
2022-10-12 12:26:08 +0300

    1

--- temp.py
+++ temp.py
@@ -1,7 +1,46 @@
 from tkinter import *
 from tkinter import ttk
 
-d = {}
+d = {
+    "1": {
+        "udk": "1",
+        "k_name": "Name1",
+        "k_genre": "Genre1",
+        "manufacturer": "Manuf1",
+        "release_year": 2000,
+        "out_date": "01.01",
+        "out_time": "10.02",
+    },
+    "2": {
+        "udk": "1",
+        "k_name": "Name1",
+        "k_genre": "Genre1",
+        "manufacturer": "Manuf1",
+        "release_year": 2000,
+        "out_date": "01.01",
+        "out_time": "10.02",
+    },
+    "3": {
+        "udk": "1",
+        "k_name": "Name1",
+        "k_genre": "Genre1",
+        "manufacturer": "Manuf1",
+        "release_year": 2000,
+        "out_date": "01.01",
+        "out_time": "10.02",
+    },
+    "4": {
+        "udk": "1",
+        "k_name": "Name1",
+        "k_genre": "Genre1",
+        "manufacturer": "Manuf4",
+        "release_year": 2000,
+        "out_date": "01.01",
+        "out_time": "10.02",
+    }
+}
+
+count = 4
 
 h1 = "УДК  кассеты"
 h2 = "Название фильма"
@@ -11,126 +50,10 @@ h5 = "Год выпуска"
 h6 = "Дата проката"
 h7 = "Время проката"
 
-ws = Tk()
-ws.title('Лабораторная работа 7')
-ws.geometry('1300x600')
 
-Label(ws, text="Список записей", font=('Helvetica', 16, "bold")).pack()
+# ЛОГИКА
 
-game_frame = Frame(ws)
-game_frame.pack()
-
-# scrollbar
-game_scroll = Scrollbar(game_frame)
-game_scroll.pack(side=RIGHT, fill=Y)
-
-game_scroll = Scrollbar(game_frame, orient='horizontal')
-game_scroll.pack(side=BOTTOM, fill=X)
-
-my_game = ttk.Treeview(game_frame, yscrollcommand=game_scroll.set, xscrollcommand=game_scroll.set)
-
-my_game.pack()
-
-game_scroll.config(command=my_game.yview)
-game_scroll.config(command=my_game.xview)
-
-# define our column
-
-my_game['columns'] = ('udk', 'k_name', 'k_genre', 'manufacturer', 'release_year', 'out_date', 'out_time')
-
-# format our column
-my_game.column("#0", width=0, stretch=NO)
-my_game.column("udk", anchor=CENTER, width=110)
-my_game.column("k_name", anchor=CENTER, width=140)
-my_game.column("k_genre", anchor=CENTER, width=90)
-my_game.column("manufacturer", anchor=CENTER, width=120)
-my_game.column("release_year", anchor=CENTER, width=100)
-my_game.column("out_date", anchor=CENTER, width=100)
-my_game.column("out_time", anchor=CENTER, width=110)
-
-# Create Headings
-my_game.heading("#0", text="", anchor=CENTER)
-my_game.heading("udk", text=h1, anchor=CENTER)
-my_game.heading("k_name", text=h2, anchor=CENTER)
-my_game.heading("k_genre", text=h3, anchor=CENTER)
-my_game.heading("manufacturer", text=h4, anchor=CENTER)
-my_game.heading("release_year", text=h5, anchor=CENTER)
-my_game.heading("out_date", text=h6, anchor=CENTER)
-my_game.heading("out_time", text=h7, anchor=CENTER)
-
-# add data
-my_game.insert(parent='', index='end', iid=0, text='',
-               values=('1', 'Ninja', '101', 'Oklahoma', 'Moore'))
-my_game.insert(parent='', index='end', iid=1, text='',
-               values=('2', 'Ranger', '102', 'Wisconsin', 'Green Bay'))
-my_game.insert(parent='', index='end', iid=2, text='',
-               values=('3', 'Deamon', '103', 'California', 'Placentia'))
-my_game.insert(parent='', index='end', iid=3, text='',
-               values=('4', 'Dragon', '104', 'New York', 'White Plains'))
-my_game.insert(parent='', index='end', iid=4, text='',
-               values=('5', 'CrissCross', '105', 'California', 'San Diego'))
-my_game.insert(parent='', index='end', iid=5, text='',
-               values=('6', 'ZaqueriBlack', '106', 'Wisconsin', 'TONY'))
-my_game.insert(parent='', index='end', iid=6, text='',
-               values=('7', 'RayRizzo', '107', 'Colorado', 'Denver'))
-my_game.insert(parent='', index='end', iid=7, text='',
-               values=('8', 'Byun', '108', 'Pennsylvania', 'ORVISTON'))
-my_game.insert(parent='', index='end', iid=8, text='',
-               values=('9', 'Trink', '109', 'Ohio', 'Cleveland'))
-my_game.insert(parent='', index='end', iid=9, text='',
-               values=('10', 'Twitch', '110', 'Georgia', 'Duluth'))
-my_game.insert(parent='', index='end', iid=10, text='',
-               values=('11', 'Animus', '111', 'Connecticut', 'Hartford'))
-my_game.pack()
-
-# добавление записей
-Label(ws, text="Добавление записей", font=('Helvetica', 16, "bold")).pack()
-Input_frame = Frame(ws)
-Input_frame.pack()
-
-udk = Label(Input_frame, text=h1)
-udk.grid(row=0, column=0)
-
-k_name = Label(Input_frame, text=h2)
-k_name.grid(row=0, column=1)
-
-k_genre = Label(Input_frame, text=h3)
-k_genre.grid(row=0, column=2)
-
-manufacturer = Label(Input_frame, text=h4)
-manufacturer.grid(row=0, column=3)
-
-release_year = Label(Input_frame, text=h5)
-release_year.grid(row=0, column=4)
-
-out_date = Label(Input_frame, text=h6)
-out_date.grid(row=0, column=5)
-
-out_time = Label(Input_frame, text=h7)
-out_time.grid(row=0, column=6)
-
-id_entry = Entry(Input_frame)
-id_entry.grid(row=1, column=0)
-
-fullname_entry = Entry(Input_frame)
-fullname_entry.grid(row=1, column=1)
-
-award_entry = Entry(Input_frame)
-award_entry.grid(row=1, column=2)
-
-award_entry = Entry(Input_frame)
-award_entry.grid(row=1, column=3)
-
-award_entry = Entry(Input_frame)
-award_entry.grid(row=1, column=4)
-
-award_entry = Entry(Input_frame)
-award_entry.grid(row=1, column=5)
-
-award_entry = Entry(Input_frame)
-award_entry.grid(row=1, column=6)
-
-def input_record():
+def remove_record():
     global count
 
     set.insert(parent='', index='end', iid=count, text='',
@@ -142,7 +65,273 @@ def input_record():
     k_genre.delete(0, END)
 
 
-# button
+def input_record():
+    set.insert(parent='', index='end', iid=count, text='',
+               values=(udk.get(), k_name.get(), k_genre.get()))
+
+    global count
+    count+1
+
+    toudk.delete(0, END)
+    tok_name.delete(0, END)
+    tok_genre.delete(0, END)
+    tomanufacturer.delete(0, END)
+    torelease_year.delete(0, END)
+    toout_date.delete(0, END)
+    toout_time.delete(0, END)
+
+# ИНТЕРФЕЙС
+
+# основные функции
+def additional_menu():
+    ws2 = Tk()
+    ws2.title('Лабораторная работа 7 - Основные функции')
+    ws2.geometry('600x400')
+
+    # удаление записей
+    Label(ws2, text="Удаление всех записей с заданным", font=('Helvetica', 16, "bold")).pack()
+    Input_frame = Frame(ws2)
+    Input_frame.pack()
+
+    # название
+    release_year = Label(Input_frame, text=h5)
+    release_year.grid(row=0, column=0)
+
+    # столбец
+    id_entry = Entry(Input_frame)
+    id_entry.grid(row=1, column=0)
+
+    Button(ws2, text="Удалить", command=input_record).pack()
+
+    # заменить удк кассеты
+    Label(ws2, text="Заменить УДК кассеты", font=('Helvetica', 16, "bold")).pack()
+    Input_frame = Frame(ws2)
+    Input_frame.pack()
+
+    # названия
+    k_name = Label(Input_frame, text=h2)
+    k_name.grid(row=0, column=0)
+
+    udk = Label(Input_frame, text=h1)
+    udk.grid(row=0, column=1)
+
+    udk_to = Label(Input_frame, text="Заменить УДК на")
+    udk_to.grid(row=0, column=2)
+
+    # столбцы
+    k_name = Entry(Input_frame)
+    k_name.grid(row=1, column=0)
+
+    udk = Entry(Input_frame)
+    udk.grid(row=1, column=1)
+
+    udk_to = Entry(Input_frame)
+    udk_to.grid(row=1, column=2)
+
+    # кнопка добавить
+    Input_button = Button(ws2, text="Применить", command=input_record)
+
+    Input_button.pack()
+
+    # все сведения по производителю
+    Label(ws2, text="Все сведения о фильмах заданного", font=('Helvetica', 16, "bold")).pack()
+    Input_frame = Frame(ws2)
+    Input_frame.pack()
+
+    manufacturer = Label(Input_frame, text=h4)
+    manufacturer.grid(row=0, column=0)
+
+    # столбец
+    manufacturer = Entry(Input_frame)
+    manufacturer.grid(row=1, column=0)
+
+    Button(ws2, text="Показать", command=lambda: show_by_manufacturer(ws2)).pack()
+
+    ws2.mainloop()
+
+
+# все сведения по производителю (таблица)
+def show_by_manufacturer(ws2):
+    Label(ws2, text="Список записей", font=('Helvetica', 16, "bold")).pack()
+
+    sbm_frame = Frame(ws2)
+    sbm_frame.pack()
+
+    # scrollbar
+    sbm_scroll = Scrollbar(sbm_frame)
+    sbm_scroll.pack(side=RIGHT, fill=Y)
+
+    sbm_scroll = Scrollbar(sbm_frame, orient='horizontal')
+    sbm_scroll.pack(side=BOTTOM, fill=X)
+
+    sbm = ttk.Treeview(sbm_frame, yscrollcommand=sbm_scroll.set, xscrollcommand=sbm_scroll.set)
+    # формат столбцов
+    sbm.column("#0", width=0, stretch=NO)
+    sbm.column("udk", anchor=CENTER, width=110)
+    sbm.column("k_name", anchor=CENTER, width=140)
+    sbm.column("k_genre", anchor=CENTER, width=90)
+    sbm.column("release_year", anchor=CENTER, width=100)
+    sbm.column("out_date", anchor=CENTER, width=100)
+    sbm.column("out_time", anchor=CENTER, width=110)
+
+    # названия столбцов
+    sbm.heading("#0", text="", anchor=CENTER)
+    sbm.heading("udk", text=h1, anchor=CENTER)
+    sbm.heading("k_name", text=h2, anchor=CENTER)
+    sbm.heading("k_genre", text=h3, anchor=CENTER)
+    sbm.heading("release_year", text=h5, anchor=CENTER)
+    sbm.heading("out_date", text=h6, anchor=CENTER)
+    sbm.heading("out_time", text=h7, anchor=CENTER)
+
+    # названия
+    udk = Label(Input_frame, text=h1)
+    udk.grid(row=0, column=0)
+
+    k_name = Label(Input_frame, text=h2)
+    k_name.grid(row=0, column=1)
+
+    k_genre = Label(Input_frame, text=h3)
+    k_genre.grid(row=0, column=2)
+
+    release_year = Label(Input_frame, text=h5)
+    release_year.grid(row=0, column=3)
+
+    out_date = Label(Input_frame, text=h6)
+    out_date.grid(row=0, column=4)
+
+    out_time = Label(Input_frame, text=h7)
+    out_time.grid(row=0, column=5)
+
+    # столбцы
+    udk = Entry(Input_frame)
+    udk.grid(row=1, column=0)
+
+    k_name = Entry(Input_frame)
+    k_name.grid(row=1, column=1)
+
+    k_genre = Entry(Input_frame)
+    k_genre.grid(row=1, column=2)
+
+    release_year = Entry(Input_frame)
+    release_year.grid(row=1, column=3)
+
+    out_date = Entry(Input_frame)
+    out_date.grid(row=1, column=4)
+
+    out_time = Entry(Input_frame)
+    out_time.grid(row=1, column=5)
+
+    # TODO delete
+    sbm.insert(parent='', index='end', iid=0, text='',
+               values=('1', 'Ninja', '101', 'Oklahoma', 'Moore'))
+
+
+# основной интерфейс
+
+ws = Tk()
+ws.title('Лабораторная работа 7')
+ws.geometry('1300x600')
+
+Label(ws, text="Список записей", font=('Helvetica', 16, "bold")).pack()
+
+main_frame = Frame(ws)
+main_frame.pack()
+
+# scrollbar
+main_scroll = Scrollbar(main_frame)
+main_scroll.pack(side=RIGHT, fill=Y)
+
+main_scroll = Scrollbar(main_frame, orient='horizontal')
+main_scroll.pack(side=BOTTOM, fill=X)
+
+main_menu = ttk.Treeview(main_frame, yscrollcommand=main_scroll.set, xscrollcommand=main_scroll.set)
+
+main_menu.pack()
+
+main_scroll.config(command=main_menu.yview)
+main_scroll.config(command=main_menu.xview)
+
+# инициализация столбцов
+
+main_menu['columns'] = ('udk', 'k_name', 'k_genre', 'manufacturer', 'release_year', 'out_date', 'out_time')
+
+# формат столбцов
+main_menu.column("#0", width=0, stretch=NO)
+main_menu.column("udk", anchor=CENTER, width=110)
+main_menu.column("k_name", anchor=CENTER, width=140)
+main_menu.column("k_genre", anchor=CENTER, width=90)
+main_menu.column("manufacturer", anchor=CENTER, width=120)
+main_menu.column("release_year", anchor=CENTER, width=100)
+main_menu.column("out_date", anchor=CENTER, width=100)
+main_menu.column("out_time", anchor=CENTER, width=110)
+
+# названия столбцов
+main_menu.heading("#0", text="", anchor=CENTER)
+main_menu.heading("udk", text=h1, anchor=CENTER)
+main_menu.heading("k_name", text=h2, anchor=CENTER)
+main_menu.heading("k_genre", text=h3, anchor=CENTER)
+main_menu.heading("manufacturer", text=h4, anchor=CENTER)
+main_menu.heading("release_year", text=h5, anchor=CENTER)
+main_menu.heading("out_date", text=h6, anchor=CENTER)
+main_menu.heading("out_time", text=h7, anchor=CENTER)
+
+# считывает словарь в таблицу
+for i in range(1, 5):
+    main_menu.insert(parent='', index='end', iid=i - 1, text='', values=(
+    d[str(i)]['udk'], d[str(i)]['k_name'], d[str(i)]['k_genre'], d[str(i)]['manufacturer'], d[str(i)]['release_year'],
+    d[str(i)]['out_date'], d[str(i)]['out_time'],))
+main_menu.pack()
+
+# добавление записей
+Label(ws, text="Добавление записей", font=('Helvetica', 16, "bold")).pack()
+Input_frame = Frame(ws)
+Input_frame.pack()
+
+# названия
+toudk = Label(Input_frame, text=h1)
+toudk.grid(row=0, column=0)
+
+tok_name = Label(Input_frame, text=h2)
+tok_name.grid(row=0, column=1)
+
+tok_genre = Label(Input_frame, text=h3)
+tok_genre.grid(row=0, column=2)
+
+tomanufacturer = Label(Input_frame, text=h4)
+tomanufacturer.grid(row=0, column=3)
+
+torelease_year = Label(Input_frame, text=h5)
+torelease_year.grid(row=0, column=4)
+
+toout_date = Label(Input_frame, text=h6)
+toout_date.grid(row=0, column=5)
+
+toout_time = Label(Input_frame, text=h7)
+toout_time.grid(row=0, column=6)
+
+# столбцы
+toudk = Entry(Input_frame)
+toudk.grid(row=1, column=0)
+
+tok_name = Entry(Input_frame)
+tok_name.grid(row=1, column=1)
+
+tok_genre = Entry(Input_frame)
+tok_genre.grid(row=1, column=2)
+
+tomanufacturer = Entry(Input_frame)
+tomanufacturer.grid(row=1, column=3)
+
+torelease_year = Entry(Input_frame)
+torelease_year.grid(row=1, column=4)
+
+toout_date = Entry(Input_frame)
+toout_date.grid(row=1, column=5)
+
+toout_time = Entry(Input_frame)
+toout_time.grid(row=1, column=6)
+
+# кнопка добавить
 Input_button = Button(ws, text="Добавить", command=input_record)
 
 Input_button.pack()
@@ -158,31 +347,15 @@ udk.grid(row=0, column=0)
 id_entry = Entry(Input_frame)
 id_entry.grid(row=1, column=0)
 
-def remove_record():
-    global count
-
-    set.insert(parent='', index='end', iid=count, text='',
-               values=(udk.get(), k_name.get(), k_genre.get()))
-    count += 1
-
-    udk.delete(0, END)
-    k_name.delete(0, END)
-    k_genre.delete(0, END)
-
-
-# button
+# кнопка удалить
 Remove_button = Button(ws, text="Удалить", command=input_record)
 
 Remove_button.pack()
 
+# кнопка основные функции
 Label(ws, text="Другое", font=('Helvetica', 16, "bold")).pack()
 
-Func_button = Button(ws, text="Открыть Основные функции", width=45, height=3, command=input_record)
+Func_button = Button(ws, text="Открыть Основные функции", width=45, height=3, command=additional_menu)
 Func_button.pack()
-
-def quit():
-    print("Have a great day! Goodbye :)")
-    sys.exit(0)
-
 
 ws.mainloop()
